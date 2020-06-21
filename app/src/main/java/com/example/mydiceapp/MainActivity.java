8f1ba9c6330c9f4b2bcddcaed004455527699def
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-21 22:33:55 +1200

    Created Array

--- app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Random;
 
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView diceImage1 = findViewById(R.id.imgDice1);
+        ImageView diceImage2 = findViewById(R.id.imgDice2);
+
+        final int [] diceImages = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6 };
+
+        //diceImages[2]
 
         int myBox = 10;
         float myFloatBox = 4.2f;
@@ -28,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
               Log.i("MyDiceApp", "btnRoll is tapped now!");
 
               Random rndObject = new Random();
-              int myRandomNumber = rndObject.nextInt(8);
+              int myRandomNumber = rndObject.nextInt(6);
               Log.i("MyDiceApp", "The generated random number is " + myRandomNumber + "");
+              diceImage1.setImageResource(diceImages[myRandomNumber]);
+
             }
         });
 
