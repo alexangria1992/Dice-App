845691a867a777dfd5b68643f9b1dc8f526bac6d
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-21 22:03:04 +1200

    Created Event Listener

--- app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mydiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int myBox = 10;
+        float myFloatBox = 4.2f;
+        double mtDoubleBox = 3.34578923;
+
+        Button btnRoll = findViewById(R.id.btnRollTheDice);
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Log.i("MyDiceApp", "btnRoll is tapped now!");
+            }
+        });
+
+
     }
 }
